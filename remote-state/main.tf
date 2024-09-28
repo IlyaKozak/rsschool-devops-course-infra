ecbf879a00ea31d109af8dada6edbe35c84a208a
Ilya Kozak <ilya.g.kozak@gmail.com>
2024-09-28 09:59:34 +0300

    feat: add remote_state init folder with s3 and dynamodb resources for terraform state and locking

---
+++ remote-state/main.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "terraform_state" {
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
