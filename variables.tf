5dbe6cd567ed42b5fafd0f0c78272b6780669172
Ilya Kozak <ilya.g.kozak@gmail.com>
2024-10-02 20:58:15 +0300

    style: terraform fmt

--- variables.tf
+++ variables.tf
@@ -22,11 +22,11 @@ variable "github" {
 variable "oidc_provider" {
   description = "github actions OIDC provider"
   type = object({
-    domain = string
+    domain      = string
     thumbprints = list(string)
   })
   default = {
-    domain = "token.actions.githubusercontent.com"
+    domain      = "token.actions.githubusercontent.com"
     thumbprints = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
   }
 }
