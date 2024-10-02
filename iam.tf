0882033ebc2ec9cab7ae46ad11fd94d93c1baade
Ilya Kozak <ilya.g.kozak@gmail.com>
2024-10-02 21:04:52 +0300

    refactor: variables to data structures

--- iam.tf
+++ iam.tf
@@ -2,14 +2,14 @@ resource "aws_iam_openid_connect_provider" "oidc" {
   url = "https://${var.oidc_provider.domain}"
 
   client_id_list = [
-    "sts.amazonaws.com",
+    var.oidc_provider.audience,
   ]
 
   thumbprint_list = var.oidc_provider.thumbprints
 }
 
 resource "aws_iam_role" "github_actions_role" {
-  name               = var.iam_role_name
+  name               = var.iam_role.name
   assume_role_policy = data.aws_iam_policy_document.policy_doc.json
 }
 
@@ -25,7 +25,7 @@ data "aws_iam_policy_document" "policy_doc" {
     condition {
       test     = "StringEquals"
       variable = "${var.oidc_provider.domain}:aud"
-      values   = ["sts.amazonaws.com"]
+      values   = [var.oidc_provider.audience]
     }
 
     condition {
@@ -37,16 +37,7 @@ data "aws_iam_policy_document" "policy_doc" {
 }
 
 resource "aws_iam_role_policy_attachment" "policy-attach" {
-  for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
-    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/IAMFullAccess",
-    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
-    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
-    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
-    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
-  ])
+  for_each = var.iam_role.policies
 
   role       = aws_iam_role.github_actions_role.name
   policy_arn = each.value
--- variables.tf
+++ variables.tf
@@ -4,10 +4,25 @@ variable "aws_region" {
   default     = "eu-north-1"
 }
 
-variable "iam_role_name" {
-  description = "iam role name for github actions"
-  type        = string
-  default     = "GithubActionsRole"
+variable "iam_role" {
+  description = "iam role for github actions"
+  type = object({
+    name     = string
+    policies = set(string)
+  })
+  default = {
+    name = "GithubActionsRole"
+    policies = [
+      "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+      "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
+      "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+      "arn:aws:iam::aws:policy/IAMFullAccess",
+      "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+      "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
+      "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
+      "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
+    ]
+  }
 }
 
 variable "github" {
@@ -23,10 +38,12 @@ variable "oidc_provider" {
   description = "github actions OIDC provider"
   type = object({
     domain      = string
+    audience    = string
     thumbprints = list(string)
   })
   default = {
     domain      = "token.actions.githubusercontent.com"
+    audience    = "sts.amazonaws.com"
     thumbprints = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
   }
 }
