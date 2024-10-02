685bb303184115b3c17e5ffdf85c1b319e6ce4ee
Ilya Kozak <ilya.g.kozak@gmail.com>
2024-10-02 21:55:03 +0300

    feat: add route tables to subnets

---
+++ route-table.tf
@@ -0,0 +1,41 @@
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.k8s-vpc.id
+  tags = {
+    Name = "public-k8s-route-table"
+  }
+}
+resource "aws_route_table_association" "public-route-1-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
+resource "aws_route_table_association" "public-route-2-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
+resource "aws_route" "public-internet-gateway-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.k8s-vpc-igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table" "private-route-table-1" {
+  vpc_id = aws_vpc.k8s-vpc.id
+  tags = {
+    Name = "private-k8s-route-table-1"
+  }
+}
+resource "aws_route_table_association" "private-route-1-association" {
+  route_table_id = aws_route_table.private-route-table-1.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+}
+
+resource "aws_route_table" "private-route-table-2" {
+  vpc_id = aws_vpc.k8s-vpc.id
+  tags = {
+    Name = "private-k8s-route-table-2"
+  }
+}
+resource "aws_route_table_association" "private-route-2-association" {
+  route_table_id = aws_route_table.private-route-table-2.id
+  subnet_id      = aws_subnet.private-subnet-2.id
+}
