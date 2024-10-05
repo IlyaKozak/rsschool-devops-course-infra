537c94e3e8bc24ce1c369658567aef23eaf22e99
Ilya Kozak <ilya.g.kozak@gmail.com>
2024-10-05 19:48:09 +0300

    fix: user data for nat instance with FORWARD chain

--- user_data_nat.sh
+++ user_data_nat.sh
@@ -4,4 +4,5 @@ sudo systemctl enable --now iptables
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.d/custom-ip-forwarding.conf
 sudo sysctl -p /etc/sysctl.d/custom-ip-forwarding.conf
 sudo /sbin/iptables -t nat -A POSTROUTING -o $(ip -br l | awk '$1 !~ "lo|vir|wl" { print $1}') -j MASQUERADE
+sudo /sbin/iptables -F FORWARD
 sudo service iptables save
